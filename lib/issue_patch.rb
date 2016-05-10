caec31d7a5b8066ad557789f4cbc94c45cd28f60
Alexander Tsirel <noma4i@gmail.com>
2016-05-10 17:31:14 +1000

    update custom field

--- lib/issue_patch.rb
+++ lib/issue_patch.rb
@@ -12,16 +12,20 @@ module IssuePatch
         project = self.project
         harbors = project.harbors
         issue_commits = self.changesets.map(&:revision)
+        is_shipped = false
         harbors.each do |h|
           h_t = self.shipped_targets.find_by_harbor_id(h.id) || h.shipped_targets.new(issue_id: self.id)
           harbor_commits = open(h.url).read.split("\n") rescue []
           if check_rules(issue_commits, harbor_commits, h.lookup_rule)
             h_t.shipped = true
+            is_shipped = false
           else
             h_t.shipped = false
           end
           h_t.save
         end
+        shipped_cf = CustomValue.joins(:custom_field).where(custom_fields: {id: custom_field_pivotal_story_description}, customized_id: issue_id).first
+        shipped_cf.update_column(:value, is_shipped)
       end
 
       protected
